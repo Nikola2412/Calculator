15135f9c9068a1dd9336d52bb83d5825627870fc
Nikola2412 <nikolamark50@gmail.com>
2022-01-14 19:12:39 +0100

    dasda
    Change

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,7 +47,6 @@
             this.button17 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -191,7 +190,7 @@
             this.button11.TabIndex = 10;
             this.button11.Text = "=";
             this.button11.UseVisualStyleBackColor = false;
-            this.button11.Click += new System.EventHandler(this.button1_Click);
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -295,25 +294,12 @@
             this.label1.TabIndex = 18;
             this.label1.Text = "label1";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(11, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 29);
-            this.label2.TabIndex = 19;
-            this.label2.Text = "label2";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(381, 420);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button17);
@@ -363,7 +349,6 @@
         private System.Windows.Forms.Button button17;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,33 +18,47 @@ namespace Calculator
         }
         double? first = null;
         double? second = null;
+        string r = null;
         string[] o = { "+", "-", "*", "/" };
+        int[] numbs = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
         string oper = null;
         private void button1_Click(object sender, EventArgs e)
         {
-            string t = ((Button)sender).Text;
-            
-            if (o.Any(p => p == t))
+            var t = ((Button)sender).Text;
+
+            textBox1.Text += t;
+
+            Test(textBox1.Text);
+        }
+        private void Test(string s)
+        {
+            int n = s.Length;
+
+            if (n != 0 && o.Any(p => p == s[n - 1].ToString()) && s!="=")
             {
-                first=double.Parse(textBox1.Text);
-                label1.Text=textBox1.Text;
-                label2.Text = $"{t}";
-                textBox1.Clear();
-                oper = t;
+                if (first == null && second == null)
+                {
+                    oper = s[n - 1].ToString();
+                    first = double.Parse(s.Remove(n - 1));
+                    textBox1.Clear();
+                    label1.Text = $"{first}";
+                }
+                else if (first != null && second == null)
+                {
+                    oper = s[n - 1].ToString();
+                    second = double.Parse(s.Remove(n - 1));
+                    r = Calculate();
+                    label1.Text = $"{r}";
+                    textBox1.Clear();
+                    first = double.Parse(r);
+                    second = null;
+                }
             }
-            else if(t=="=")
-            {
-                second = double.Parse(textBox1.Text);
-                label1.Text = "";
-                label2.Text="";
-                textBox1.Text = Calculate();
-            }
-            else
-                textBox1.Text += t;
+
         }
         private string Calculate()
         {
-            double? res=null;
+            double? res = null;
             switch (oper)
             {
                 case "+":
@@ -67,13 +81,57 @@ namespace Calculator
         {
             first = null;
             second = null;
+            r = null;
+            label1.Text = "";
             textBox1.Clear();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             label1.Text = "";
-            label2.Text = "";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                second = double.Parse(textBox1.Text);
+                textBox1.Text = Calculate();
+            }
+            else
+            {
+                textBox1.Text = first.ToString();
+            }
+            label1.Text = "";
+            first = null;
+            second = null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            Test(s);
+        }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13)
+            {
+                if (textBox1.Text != "")
+                {
+                    second = double.Parse(textBox1.Text);
+                    textBox1.Text = Calculate();
+                }
+                else
+                {
+                    textBox1.Text = first.ToString();
+                }
+                label1.Text = "";
+                first = null;
+                second = null;
+
+            }
         }
     }
 }
