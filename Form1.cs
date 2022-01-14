9c6de76d5724cbf17a7dcfc6021b4b4697eb3dd4
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2022-01-14 01:11:22 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double? first = null;
+        double? second = null;
+        string[] o = { "+", "-", "*", "/" };
+        string oper = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string t = ((Button)sender).Text;
+            
+            if (o.Any(p => p == t))
+            {
+                first=double.Parse(textBox1.Text);
+                label1.Text=textBox1.Text;
+                label2.Text = $"{t}";
+                textBox1.Clear();
+                oper = t;
+            }
+            else if(t=="=")
+            {
+                second = double.Parse(textBox1.Text);
+                label1.Text = "";
+                label2.Text="";
+                textBox1.Text = Calculate();
+            }
+            else
+                textBox1.Text += t;
+        }
+        private string Calculate()
+        {
+            double? res=null;
+            switch (oper)
+            {
+                case "+":
+                    res = first + second;
+                    break;
+                case "-":
+                    res = first - second;
+                    break;
+                case "/":
+                    res = first / second;
+                    break;
+                case "*":
+                    res = first * second;
+                    break;
+            }
+            return res.ToString();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            first = null;
+            second = null;
+            textBox1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+    }
+}
